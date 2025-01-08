ad821fc4314b4d289de6f536f261ca8b9ce9be13
Mbolot349 <maksymbolotovstudent@gmail.com>
2025-01-08 12:16:40 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-
+print("=========================part1 executing==========================")
 # Construct the path to svm_part1.py
 script_path = os.path.join('A', 'svm_part1.py')
 
@@ -8,18 +8,19 @@ script_path = os.path.join('A', 'svm_part1.py')
 
 print("running the SVM Model for part 1")
 subprocess.run(['python', script_path])
-print("SVM model finished")
+print("============================================SVM model finished==============================================")
 
 
 
-
-script_path_1 = os.path.join('A', 'random_forest_final.py')
-print("running random forest model for task 1")
+script_path_1 = os.path.join('A', 'random_forest_part1_load.py')
+print("running random forest model for task 1, loaded to save time when executing the code")
+print("================================See A/random_forest_final.py for model training==================================")
 subprocess.run(['python', script_path_1])
-print("Random Forest model finished")
+print("==================================Random Forest model finished=====================================================================")
 
 script_path_2 = os.path.join('A', 'load.py')
 print("Final Deep Learning Model Loading")
 print("loading CNN model and testing")
 subprocess.run(['python', script_path_2])
-print("TASK 1 Results loaded")
+print("TASK 1 CNN Results loaded")
+print("=================================TASK 1 IS FINISHED ============================================")
