2bfbc78fc47d7da596c386c1be9bed4760d26b14
Mbolot349 <maksymbolotovstudent@gmail.com>
2025-01-08 11:46:53 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+
+# Construct the path to svm_part1.py
+script_path = os.path.join('A', 'svm_part1.py')
+
+# Call the script with python
+subprocess.run(['python', script_path])
