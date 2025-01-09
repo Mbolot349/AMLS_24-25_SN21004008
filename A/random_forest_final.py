c59a12607e982141e1320f3b12f5ba996235815c
Mbolot349 <maksymbolotovstudent@gmail.com>
2025-01-09 11:04:38 +0000

    Update random_forest_final.py

--- A/random_forest_final.py
+++ A/random_forest_final.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import  GridSearchCV
 from sklearn.metrics import (
     accuracy_score,
     classification_report,
