4091b8571a48682434051e0a9e20437bcd5ae7de
Mbolot349 <maksymbolotovstudent@gmail.com>
2025-01-10 13:29:16 +0000

    Update neural_final.py

--- A/neural_final.py
+++ A/neural_final.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Activation
 from tensorflow.keras.callbacks import EarlyStopping
